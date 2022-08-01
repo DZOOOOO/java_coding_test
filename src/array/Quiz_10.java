886920709cb376735c377c4a8bb01cc13345189e
dzoo <dzpro0327@naver.com>
2022-08-01 10:43:50 +0900

    11

---
+++ src/array/Quiz_10.java
@@ -0,0 +1,43 @@
+package array;
+
+import java.util.Scanner;
+
+public class Quiz_10 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println(solution(n, arr));
+    }
+
+    public static int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k]; // -1 0 1 0
+                    int ny = j + dy[k]; // 0 1 0 -1
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/array/Quiz_9.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.Scanner;
+
+public class Quiz_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    public static int solution(int n, int[][] arr) {
+        int answer = 0;
+        // 행
+        int sum1;
+        // 열
+        int sum2;
+
+        for (int i = 0; i < n; i++) {
+            sum1 = 0;
+            sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        sum1 = 0;
+        sum2 = 0;
+
+        // 대각선 구하기
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+}
